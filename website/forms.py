d075e4bad948747f6196078503a7768f84f867e0
Jason Scharmann <scharmann.jason@protonmail.com>
2023-06-29 11:17:41 -0700

    User Login and Registration

---
+++ website/forms.py
@@ -0,0 +1,52 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
+with open("form_help_text/username_help_text.html") as file:
+    username_help_text = file.read()
+with open("form_help_text/password1_help_text.html") as file:
+    password1_help_text = file.read()
+with open("form_help_text/password2_help_text.html") as file:
+    password2_help_text = file.read()
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(
+        Label="",
+        widget=forms.TextInput(attrs={'class': 'form-control',
+                                      'placeholder': 'Email Address'}))
+
+    first_name = forms.CharField(
+        Label="",
+        max_length=100,
+        widget=forms.TextInput(attrs={'class': 'form-control',
+                                      'placeholder': 'First Name'}))
+
+    last_name = forms.CharField(
+        Label="",
+        max_length=100,
+        widget=forms.TextInput(attrs={'class': 'form-control',
+                                      'placeholder': 'Last Name'}))
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name',
+                  'last_name', 'email',
+                  'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['username'].widget.attrs['placeholder'] = 'User Name'
+        self.fields['username'].label = ''
+        self.fields['username'].help_text = username_help_text
+
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['placeholder'] = 'Password'
+        self.fields['password1'].label = ''
+        self.fields['password1'].help_text = password1_help_text
+
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
+        self.fields['password2'].label = ''
+        self.fields['password2'].help_text = password2_help_text
--- website/views.py
+++ website/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+def home(request):
+    # Check if logged in
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        # Authenticate
+        user = authenticate(request, username = username, password = password)
+        return check_user_exists(request, user)
+
+    return render(request, 'home.html', {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You Have Been Logged Out...")
+    return redirect('home')
+
+def register_user(request):
+    return render(request, "register.html", {})
+
+# Utility Functions -------------------------------------------------------
+
+def check_user_exists(request, user):
+    if user is not None:
+        login(request, user)
+        return show_message(request, "You Have Been Logged In!")
+    else:
+        return show_message(request,
+                            "There was an error, please try again.")
+
+def show_message(request, msg):
+    messages.success(request, msg)
+    return redirect('home')
